b69790aff0a4aca63c14bea85ab8a4daf58e1f5e
Svetlana Trukhina <sweta4ca@mail.ru>
2022-07-02 06:35:09 +0700

    feat: done the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const param = String(n).includes('-')
+        ? String(n).slice(1) 
+        : String(n)
+    return +(param.split('').reverse().join(''))
 }
